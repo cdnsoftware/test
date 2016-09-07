ef568532de8756fa999a3caee7517dfc28709a39
chandramani prasad <chandramaniprasad@cdnsol.com>
2016-09-07 09:47:01 +0530

    Task 2

---
+++ SampleApCore/ViewModels/vmICommon.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleApCore.ViewModels
+{
+    public class vmICommon
+    {
+        public vmICommon()
+        { 
+        
+        }
+
+        public enum eDisplayAs
+        {
+            FirstLast = 1,
+            LastFirst = 2,
+            Knickname = 3
+        }
+    }
+}
