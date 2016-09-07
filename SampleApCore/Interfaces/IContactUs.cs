5dee21f0e05024b9b4442d474cd1d6f21431965c
chandramani prasad <chandramaniprasad@cdnsol.com>
2016-09-07 11:25:04 +0530

    Last Task

--- SampleApCore/Interfaces/IContactUs.cs
+++ SampleApCore/Interfaces/IContactUs.cs
@@ -11,8 +11,10 @@ namespace SampleApCore.Interfaces
 {
     public interface IContactUs
     {
+        //List<IGrouping<string,vmContactList>>
         List<Contact> GetAllContacts(int id = 0);
-        List<vmContactList> GetAllNewTestContacts(int id = 0);
+        //List<vmContactList> GetAllNewTestContacts(int id = 0);
+        List<IGrouping<string, vmContactList>> GetAllNewTestContacts(int id = 0);
         vmTransactionResult<Contact> CreateContact(Contact model);
         vmTransactionResult<bool> DeleteContact(int id);
     }
