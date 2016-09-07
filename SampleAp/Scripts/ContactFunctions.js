c97acc3360cdef62f601eebe4290e7e5c45758e4
chandramani prasad <chandramaniprasad@cdnsol.com>
2016-09-07 10:12:28 +0530

    task 3

---
+++ SampleAp/Scripts/ContactFunctions.js
@@ -0,0 +1,31 @@
+﻿$(document).ready(function () {
+    $("#MasterDisplayAsId").change(function () {
+        debugger;
+        var selecetdId = $(this).val();
+        if (selecetdId != "") {
+
+            var RFN = $("#FirstName").val();
+            var RLN = $("#LastName").val();
+            var RKN = $("#Knickname").val();
+
+
+            var varDisplayAs = "";
+
+            if (selecetdId == 1) {
+                varDisplayAs = RFN + " " + RLN;
+            }
+            else if (selecetdId == 2) {
+                varDisplayAs = RLN + " " + RFN;
+            }
+            else if (selecetdId == 3) {
+                varDisplayAs = RKN
+            }
+
+            $("#Preview").text(varDisplayAs);
+        }
+        else {
+            $("#Preview").text("");
+        }
+
+    });
+});
