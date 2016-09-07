50c8a9bfb4bfb86536c42267c7238752f045a03c
chandramani prasad <chandramaniprasad@cdnsol.com>
2016-09-07 09:22:01 +0530

    Base controller for exception handling

---
+++ SampleAp/Controllers/BaseController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Filters;
+using System.Web.Mvc;
+
+namespace SampleAp.Controllers
+{
+    public class BaseController : Controller 
+    {
+
+        protected override void OnException(ExceptionContext filterContext)
+        {
+            Exception exception = filterContext.Exception;
+            //Logging the Exception
+            filterContext.ExceptionHandled = true;
+
+
+            var Result = this.View("Error", new HandleErrorInfo(exception,
+                filterContext.RouteData.Values["controller"].ToString(),
+                filterContext.RouteData.Values["action"].ToString()));
+
+            filterContext.Result = Result;
+
+        }
+    }
+}
